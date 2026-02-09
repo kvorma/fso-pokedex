d8c5ff097e1c37f0f700aac38167971590a03fe5
Kai Vorma <kai.vorma@iki.fi>
2026-02-09 15:49:24 +1100

    reduced bandwidth/increased timeouts for airplane wifi

--- playwright.config.js
+++ playwright.config.js
@@ -16,13 +16,13 @@ export default defineConfig({
   testDir: './e2e-tests',
   timeout: 30000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
