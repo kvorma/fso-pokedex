85ef8d049b25ee6002614ec99d5cb7a203de8366
Kai Vorma <kai.vorma@iki.fi>
2026-02-09 15:53:31 +1100

    11.9

--- e2e-tests/simple.spec.js
+++ e2e-tests/simple.spec.js
@@ -8,7 +8,7 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  test.only('Kakuna character page can be opened', async ({ page }) => {
+  test('Kakuna character page can be opened', async ({ page }) => {
     await page.getByText('kakuna').click()
     await page.waitForURL('**/kakuna');
     await expect(page.getByText('Shed Skin')).toBeVisible()
