80b7a8c8385d3a39fa788ae51c52fb7a95499cea
Kai Vorma <kai.vorma@iki.fi>
2026-02-09 15:16:26 +1100

    11.9

--- e2e-tests/simple.spec.js
+++ e2e-tests/simple.spec.js
@@ -1,9 +1,16 @@
 const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+  })
   test('front page can be opened', async ({ page }) => {
-    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test.only('Kakuna character page can be opened', async ({ page }) => {
+    await page.getByText('kakuna').click()
+    await page.waitForURL('**/kakuna');
+    await expect(page.getByText('Shed Skin')).toBeVisible()
   })
 })
--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './e2e-tests',
+  timeout: 30000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,13 +22,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:5050',
+    baseURL: 'http://localhost:5050/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -35,7 +36,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:5050',
-    timeout: 120 * 1000,
+    timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   /* Configure projects for major browsers */
